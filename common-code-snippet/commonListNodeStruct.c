9ba74a7872fe9b1e35ead706c4f12bc6ffbe541d
psklf <surprised46@163.com>
2017-02-03 12:20:11 +0800

    Modify listnode common file

--- common-code-snippet/commonListNodeStruct.c
+++ common-code-snippet/commonListNodeStruct.c
@@ -2,13 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct Lnode {
-    char *data;
-    int length;
-    struct Lnode *next;
-} ListNode;
+typedef struct ListNode ListNode;
 
 int main() {
+
+    ListNode *node = (ListNode *) malloc(sizeof(ListNode));
+    ListNode *node1 = (ListNode *) malloc(sizeof(ListNode));
+    ListNode *node2 = (ListNode *) malloc(sizeof(ListNode));
+    ListNode *node3 = (ListNode *) malloc(sizeof(ListNode));
+    ListNode *node4 = (ListNode *) malloc(sizeof(ListNode));
+    ListNode *node5 = (ListNode *) malloc(sizeof(ListNode));
+    ListNode *node6 = (ListNode *) malloc(sizeof(ListNode));
+    node->val = 0;
+    node->next = node1;
+    node1->next = node2;
+    node2->next = NULL;
+    node1->val = 7;
+    node2->val = 2;
+    node3->val = 3;
+
+    // another list
+    node3->next = node4;
+    node4->next = node5;
+    node5->next = node6;
+    node6->next = NULL;
+    node4->val = 4;
+    node5->val = 5;
+    node6->val = 6;
+
+    // empty list
+    ListNode *nodeEmpty = (ListNode *) malloc(sizeof(ListNode));
+    nodeEmpty->val = 0;
+    nodeEmpty->next = NULL;
 
     return 0;
 }
